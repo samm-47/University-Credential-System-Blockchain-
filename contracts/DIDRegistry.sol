6414e530ae795d5f590bb479868a50302567a16e
Samir Patel <96743711+samm-47@users.noreply.github.com>
2026-03-26 20:41:35 -0700

    Code P1 added into DIDRegistry.sol

--- contracts/DIDRegistry.sol
+++ contracts/DIDRegistry.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
 
+/// @title DID Registry Contract
+/// @notice This contract will manage decentralized identifiers and the public keys
+contract DIDRegistry {
+
+    struct DID {
+            address owner;
+            string publicKey;
+            bool exists;
+        }
+
+
+
+
+}
